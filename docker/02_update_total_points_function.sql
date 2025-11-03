9b327c34a0d6e1f6050596032f74e54725037c5f
Lucas STEPHAN <lucas.stephan.12@outlook.fr>
2025-11-03 13:52:31 +0100

    fix(ranking): update user with no points

--- docker/02_update_total_points_function.sql
+++ docker/02_update_total_points_function.sql
@@ -3,11 +3,18 @@ RETURNS VOID AS
 $$
 BEGIN
     INSERT INTO ranking (rank, user_id, points)
-    SELECT 0, p.user_id, SUM(p.amount) AS total_points
-    FROM points p
-    GROUP BY p.user_id
+    SELECT 
+        0 AS rank, 
+        u.user_id, 
+        COALESCE(SUM(p.amount), 0) AS total_points
+    FROM 
+        (SELECT DISTINCT user_id FROM points) u
+    LEFT JOIN 
+        points p ON u.user_id = p.user_id
+    GROUP BY 
+        u.user_id
     ON CONFLICT (user_id)
-    DO UPDATE SET points = EXCLUDED.points;
+    DO UPDATE SET points = COALESCE(EXCLUDED.points, 0);
 
     WITH ranked_points AS (
         SELECT user_id, DENSE_RANK() OVER (ORDER BY points DESC, user_id ASC) AS rank_value
@@ -17,5 +24,9 @@ BEGIN
     SET rank = ranked_points.rank_value
     FROM ranked_points
     WHERE ranking.user_id = ranked_points.user_id;
+
+    UPDATE ranking
+    SET points = 0
+    WHERE user_id NOT IN (SELECT DISTINCT user_id FROM points);
 END;
 $$ LANGUAGE plpgsql;
