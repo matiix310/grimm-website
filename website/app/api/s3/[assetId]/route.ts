6c50bae517110778435d043109c7d42239cf4d5e
Matiix310 <lucas.stephan.12@outlook.fr>
2025-11-19 22:51:22 +0100

    5 assets managment (#9)
    * feat(garaga): setup garage docker and api endpoint
    
    * fix(lockfile): bun lockfile update
    
    * fix(nginx): removed default_server
    
    * fix(nginx): default_server and _ instead of fqdn
    
    * fix(nginx): increase body max size
    
    * fix(s3): use path style presigned urls
    
    * style(s3): removed unused import

---
+++ website/app/api/s3/[assetId]/route.ts
@@ -0,0 +1,33 @@
+// app/api/images/route.ts
+import { NextRequest, NextResponse } from "next/server";
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import ApiResponse from "@/lib/apiResponse";
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+  },
+  region: "garage",
+  endpoint: process.env.S3_URL!,
+  forcePathStyle: true,
+});
+
+export async function GET(request: NextRequest, ctx: RouteContext<"/api/s3/[assetId]">) {
+  try {
+    const assetId = (await ctx.params).assetId;
+
+    const command = new GetObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME!,
+      Key: assetId,
+    });
+
+    const url = await getSignedUrl(s3, command);
+
+    return NextResponse.redirect(url);
+  } catch (error) {
+    console.error("Error fetching image from S3:", error);
+    return ApiResponse.internalServerError();
+  }
+}
